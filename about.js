be564ee3c59d63abda1a2b2750fc3377c0d2070f
abdulaleem1122 <abdulaleem03410@gmail.com>
2024-11-19 23:29:43 +0500

    Alert

---
+++ about.js
@@ -0,0 +1,6 @@
+var a =(" Wellcome to JS Land..");
+var b = ("Happy Coding..\n prevent this page form creating additional diologs");
+var c = ("Hello.. I can run JS through my web browser's console");
+alert(a);
+alert(b);
+alert( c);
---
+++ index.js
@@ -0,0 +1,9 @@
+
+var a = ("Error! please enter a valid password");
+var b = ("Welcomen to JS Land...\n Happy Coding");
+alert(a);
+alert(b);
+document.write(a);
+
+
+
